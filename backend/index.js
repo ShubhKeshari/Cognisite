1c8ae66973b9c938f0dfd783d37e332a256c53ee
ShubhKeshari <shubh1999keshari@gmail.com>
2024-07-17 21:13:22 +0530

    Backend initialised

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { connectDB } = require("./utils/db.config");
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json({ message: "Server Home Page" });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+});
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log(`server is running at http://localhost:${PORT}`);
+});
